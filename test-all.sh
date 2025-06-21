2acd4578a875114296663867fd6e6fce84c4867d
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-06-21 07:59:06 +0530

    chore: Run GH Actions for PRs
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

---
+++ test-all.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+
+cd ..
+
+echo "============== Test config-server =================="
+cd config-server
+./mvnw spotless:apply clean verify
+cd ..
+
+echo "============== Test api-gateway =================="
+cd api-gateway
+./mvnw spotless:apply clean verify
+cd ..
+
+echo "============== Test feature-service =================="
+cd feature-service
+./mvnw spotless:apply clean verify
+cd ..
+
+echo "============== Test user-service =================="
+cd user-service
+./mvnw spotless:apply clean verify
+cd ..
+
+echo "============== Test notification-service =================="
+cd notification-service
+./gradlew spotlessApply clean test
+cd ..
+
+echo "============== Test feature-tracker-angular =================="
+cd feature-tracker-angular
+npm install
+npm test
+cd ..
