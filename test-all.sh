ac6038a6d3b8bf9b2212652ead4f531ca4d78077
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2025-06-21 08:43:37 +0530

    chore: turn of testing for angular module
    Signed-off-by: K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>

--- test-all.sh
+++ test-all.sh
@@ -30,8 +30,8 @@ cd notification-service
 ./gradlew spotlessApply clean test
 cd ..
 
-echo "============== Test feature-tracker-angular =================="
-cd feature-tracker-angular
-npm install
-npm test
-cd ..
+#echo "============== Test feature-tracker-angular =================="
+#cd feature-tracker-angular
+#npm install
+#npm test
+#cd ..
